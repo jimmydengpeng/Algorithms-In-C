ec35235959153498f683d5fd7a8061a70d08a4ab
jimmydengpeng <121dengpeng@163.com>
2020-11-17 00:20:26 +0800

    completed: coin-flipping-simu.

--- chap03/p3.7_coin-flipping-simulation.c
+++ chap03/p3.7_coin-flipping-simulation.c
@@ -1,8 +1,28 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
+int heads() {
+    return rand() <= RAND_MAX/2;
+}
 
-    printf("hello world!\n");
+int main(int argc, char *argv[]) {
+    int i, j, cnt;
+    int N = atoi(argv[1]), M = atoi(argv[2]);
+    int *f = malloc((N+1)*sizeof(int));
+
+    for (j = 0; j <= N; j++) f[i] = 0;
+
+    for (i = 0; i < M; i++, f[cnt]++)
+        for (cnt = 0, j = 0; j <= N; j++)
+            if (heads())
+                cnt++;
+
+    for (j = 0; j <= N; j++) {
+        printf("%2d ", j); // N = 32时，整型数字的打印宽度为2位
+        for (i = 0; i < f[j]; i += 10)
+            printf("*");
+        printf("\n");
+    }
+
     return 0;
 }
